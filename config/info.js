86550a39fb0d58843b54c29025aeb7c90a6c6330
Bajojajo-xD <81306360+Bajojajo-xD@users.noreply.github.com>
2021-05-24 16:59:44 +0200

    update

--- config/info.js
+++ config/info.js
@@ -2,7 +2,7 @@ module.exports = {
 
     beta: false,
 
-    releasedate: "23.05.21",
-    version: "0.15",
+    releasedate: "24.05.21",
+    version: "0.15~1",
     news: "Granie na vc już dostępne, użyj .help po więcej!",
 };
--- events/player/trackStart.js
+++ events/player/trackStart.js
@@ -5,6 +5,6 @@ module.exports = (client, message, track) => {
     .setColor(`GREEN`)
     .setTitle(`  Teraz odtwarzam \`${track.title}\`...`)
     .setThumbnail(`https://cdn.discordapp.com/attachments/837601267827998770/845348342439608380/nutka.gif`)
-    .setDescription(`${client.emotes.rbverify} **...na kanale ${message.member.voice.channel}**`)
+    .setDescription(`${client.emotes.rbverify} **...na kanale ${message.guild.me.voice.channel}**`)
     message.channel.send(embed)
 };
