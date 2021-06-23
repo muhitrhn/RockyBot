66174acebc2ae2b3dea6c920bc2315fb62ad6845
Bajojajo-xD <81306360+Bajojajo-xD@users.noreply.github.com>
2021-06-23 20:01:09 +0200

    Update info.js

--- config/info.js
+++ config/info.js
@@ -2,5 +2,6 @@ module.exports = {
     releasedate: '23.06.21',
     version: '0.24',
     author: '643084165209784331',
-    changelog: 'https://github.com/Bajojajo-xD/RockyBot/blob/main/CHANGELOG.MD'
+    changelog: 'https://github.com/Bajojajo-xD/RockyBot/blob/main/CHANGELOG.MD',
+    github: 'https://github.com/Bajojajo-xD/RockyBot/'
 }
