77ae474140fb03b3562dfe4fb434d512416a69b3
Łukasz <81306360+Bajojajo-xD@users.noreply.github.com>
2021-07-18 19:09:33 +0200

    update

--- config/info.js
+++ config/info.js
@@ -1,6 +1,6 @@
 module.exports = {
-    releasedate: '16.07.21',
-    version: '0.33',
+    releasedate: '18.07.21',
+    version: '0.33~1',
     author: '643084165209784331',
     changelog: 'https://github.com/Bajojajo-xD/RockyBot/blob/main/CHANGELOG.MD',
     github: 'https://github.com/Bajojajo-xD/RockyBot/',
